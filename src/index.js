9b6dee53519d009f0dfc58080523d5b32c708c8a
PatrickAngrezani <pangrezani@gmail.com>
2024-04-24 00:22:11 -0300

    feat: configure enablecam method

--- src/index.js
+++ src/index.js
@@ -12,8 +12,30 @@ ENABLE_CAM_BUTTON.addEventListener("click", enableCam);
 TRAIN_BUTTON.addEventListener("click", trainAndPredict);
 RESET_BUTTON.addEventListener("click", reset);
 
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
 function enableCam() {
-  // TODO: Fill this out later in the codelab!
+  if (hasGetUserMedia) {
+    //get user parameters
+    const constraints = {
+      video: true,
+      width: 640,
+      height: 480,
+    };
+
+    //activate the webcam stream
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+      VIDEO.srcObject = stream;
+      VIDEO.addEventListener("loadeddata", function () {
+        videoPlaying = true;
+        ENABLE_CAM_BUTTON.classList.add("removed");
+      });
+    });
+  } else {
+    console.warn("getUserMedia() is not supported by your browser");
+  }
 }
 
 function trainAndPredict() {
@@ -56,9 +78,30 @@ async function loadMobileNetFeatureModel() {
     let answer = mobilenet.predict(
       tf.zeros[(1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3)]
     );
-    console.log(answer.shape);
+    console.log({ asnswer: answer.shpe });
   });
 }
 
 //call function immediately to start loading
 loadMobileNetFeatureModel();
+
+let model = tf.sequential();
+console.log({ model });
+model.add(
+  tf.layers.dense({ inputShape: [1024], units: 128, activation: "relu" })
+);
+model.add(tf.layers.dense({ units: 2, activation: "softmax" }));
+
+model.summary();
+
+//compile the model with the defined optimizer and specify a loss fuction to use
+model.compile({
+  //adam changes the learning rate over time which is useful
+  optimizer: "adam",
+  //use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
+  //else categoricalCrossentropy is used if more than 2 classes
+  loss:
+    CLASS_NAMES.length === 2 ? "binaryCrossentropy" : "categoricalCrossentropy",
+  //as this is a classification problem you can record accuracy in the logs too!
+  metrics: ["accuracy"],
+});
